80bd062999a4b92e32b37f107e0e490645b022c6
madoodia <madoodia@gmail.com>
2014-04-24 10:20:45 +0430

    A little change

--- src/qml/mainLauncher.py
+++ src/qml/mainLauncher.py
@@ -1,5 +1,6 @@
 ## abstract factory ltd.
 ## Installer (launcher)
+# https://github.com/abstractfactory/installer
 ####################################################################
 import sys
 from PyQt5.QtCore import QUrl
