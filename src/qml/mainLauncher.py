317333b41a081570c8bbbf55a27b6e1eddb256c9
Marcus Ottosson <marcus@abstractfactory.io>
2014-04-24 06:53:08 +0100

    Adding env

--- src/qml/mainLauncher.py
+++ src/qml/mainLauncher.py
@@ -1,6 +1,13 @@
 ## abstract factory ltd.
 ## Installer (launcher)
 ####################################################################
+
+import os
+import PyQt5
+dirname = os.path.dirname(PyQt5.__file__)
+plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
 import sys
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QGuiApplication
