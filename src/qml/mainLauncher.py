cc9ec82a2dd263d3c8b45eb6a18c0afc07e1ed35
Marcus Ottosson <marcus@abstractfactory.io>
2014-04-24 09:15:50 +0100

    Resetting py

--- src/qml/mainLauncher.py
+++ src/qml/mainLauncher.py
@@ -3,12 +3,6 @@
 # https://github.com/abstractfactory/installer
 ####################################################################
 
-import os
-import PyQt5
-dirname = os.path.dirname(PyQt5.__file__)
-plugin_path = os.path.join(dirname, 'plugins', 'platforms')
-os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
-
 import sys
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QGuiApplication
